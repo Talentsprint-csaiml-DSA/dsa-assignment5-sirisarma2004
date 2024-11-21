6c688c6376bbf254b2ecabac7c2329277fee9340
sirisarma2004 <sarma_eie@yahoo.com>
2024-11-21 13:29:56 +0530

    1

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 def min_coins(coins, target_amount):
-    ""code here""
+  dp = [float("inf") for i in range(target_amount + 1)]
+  dp[0] = 0
+  for coin in coins:
+    count = coin
+    while (count <= target_amount):
+      if dp[count] > dp[count - coin] + 1:
+        dp[count] = dp[count - coin] + 1
+      count += 1
+  return dp.pop()
